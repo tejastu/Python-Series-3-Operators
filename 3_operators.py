40cbd93c7c2be07efe6ccb4315f222d8a622a969
Tejas Parab <62276215+tejastu@users.noreply.github.com>
2021-01-13 10:22:51 +0530

    Add files via upload

---
+++ 3_operators.py
@@ -0,0 +1,29 @@
+a = 6
+b = 2
+
+#  arithmetic operator
+print("value of a+b is ", a+b)
+print("value of a-b is ", a-b)
+print("value of a*b is ", a*b)
+print("value of a/b is ", a/b)
+
+#assignment operator
+p = 21
+p +=4   # 21 + 4
+print(p)
+ 
+#comparison operator
+x = (3>9)  #returns boolean value True/False
+print(x)
+
+#logical operator
+bool1 = True
+bool2 = False
+print('the value of bool1 and bool2 is', (bool and bool2))
+print('the value of bool1 or bool2 is', (bool or bool2))
+print('the value of not bool2 is', (not bool2))
+
+h=34
+j=80
+comp = (h>j)
+print(comp)
